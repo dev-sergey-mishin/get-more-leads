4c692c7abcdb12dbc2ce67c6d75a15611a74a342
sergey <sergey.mishin.71@gmail.com>
2017-08-15 13:00:49 +0300

    завершил верстку

--- frontend/entry.js
+++ frontend/entry.js
@@ -1,11 +1,7 @@
 require('./less/import.less');
 require('./js/slider.js');
 require('./js/form.js');
+require('./js/blog-form.js');
 require('./js/btn-container.js');
 require('./js/modal-form.js');
 require('./js/metrica.js');
-
-//require('./js/conversion.js');
-//require('./js/call-back-form.js');
-//require('./js/service-form.js');
-//require('./js/strategy-form.js');
---
+++ frontend/js/blog-form.js
@@ -0,0 +1,27 @@
+var Submit = require('./submit');
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+function submit($form, $submit, formName) {
+    var $email = $form.find('#blog-form-mail');
+
+    if (!validateEmail($email.val())) {
+        $email.addClass('error');
+    } else {
+        $email.removeClass('error');
+        Submit.submit($submit, '', '', $email.val(), { form: formName });
+    }
+}
+
+
+var $submit = $('#blog-form-submit');
+var $checkbox = $('#blog-form-accept');
+
+$checkbox.click(function() {
+    $submit.attr('disabled', !document.getElementById('blog-form-accept').checked);
+});
+$submit.click(function() {
+    submit($('.blog-form'), $(this), 'Подписка с блога');
+});
