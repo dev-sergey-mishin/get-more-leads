3e3f0c147be6abd6155062a42b02159997726ac0
sergey <sergey.mishin.71@gmail.com>
2017-09-03 12:46:57 +0300

    фикс js ошибки

--- frontend/js/blog-form.js
+++ frontend/js/blog-form.js
@@ -30,15 +30,16 @@ $submit.click(function() {
 $(document).ready(function() {
     var navbar =  $('.left-container');
     var wrapper = $('.blog');
+    if (navbar.length && wrapper.length) {
+        $(window).scroll(function(){
+            var nsc = $(document).scrollTop();
+            var bp1 = wrapper.offset().top;
+            var bp2 = bp1 + wrapper.outerHeight()-$(window).height();
 
-    $(window).scroll(function(){
-        var nsc = $(document).scrollTop();
-        var bp1 = wrapper.offset().top;
-        var bp2 = bp1 + wrapper.outerHeight()-$(window).height();
-
-        if (nsc>bp1) {  navbar.css('position','fixed'); }
-        else { navbar.css('position','absolute'); }
-        if (nsc>bp2) { navbar.css('top', bp2-nsc); }
-        else { navbar.css('top', '0'); }
-    });
+            if (nsc>bp1) {  navbar.css('position','fixed'); }
+            else { navbar.css('position','absolute'); }
+            if (nsc>bp2) { navbar.css('top', bp2-nsc); }
+            else { navbar.css('top', '0'); }
+        });
+    }
 });
