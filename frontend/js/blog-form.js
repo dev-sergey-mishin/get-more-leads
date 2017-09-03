2da6a7c402c0d67f0c401f2fda6db007506b5d51
sergey <sergey.mishin.71@gmail.com>
2017-09-03 12:37:41 +0300

    починил скролл

--- frontend/js/blog-form.js
+++ frontend/js/blog-form.js
@@ -25,3 +25,20 @@ $checkbox.click(function() {
 $submit.click(function() {
     submit($('.blog-form'), $(this), 'Подписка с блога');
 });
+
+//scroll
+$(document).ready(function() {
+    var navbar =  $('.left-container');
+    var wrapper = $('.blog');
+
+    $(window).scroll(function(){
+        var nsc = $(document).scrollTop();
+        var bp1 = wrapper.offset().top;
+        var bp2 = bp1 + wrapper.outerHeight()-$(window).height();
+
+        if (nsc>bp1) {  navbar.css('position','fixed'); }
+        else { navbar.css('position','absolute'); }
+        if (nsc>bp2) { navbar.css('top', bp2-nsc); }
+        else { navbar.css('top', '0'); }
+    });
+});
--- public/bundle.js
+++ public/bundle.js
@@ -1964,6 +1964,22 @@
 	    submit($('.blog-form'), $(this), 'Подписка с блога');
 	});
 
+	$(document).ready(function() {
+	    var navbar =  $('.left-container');
+	    var wrapper = $('.blog');
+
+	    $(window).scroll(function(){
+	        var nsc = $(document).scrollTop();
+	        var bp1 = wrapper.offset().top;
+	        var bp2 = bp1 + wrapper.outerHeight()-$(window).height();
+
+	        if (nsc>bp1) {  navbar.css('position','fixed'); }
+	        else { navbar.css('position','absolute'); }
+	        if (nsc>bp2) { navbar.css('top', bp2-nsc); }
+	        else { navbar.css('top', '0'); }
+	    });
+	});
+
 /***/ },
 /* 10 */
 /***/ function(module, exports, __webpack_require__) {
