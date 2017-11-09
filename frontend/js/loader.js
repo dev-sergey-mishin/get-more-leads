1d17bb9eb999a63c521e0e66c7e32b75da5da36b
sergey <sergey.mishin.71@gmail.com>
2017-11-09 20:25:10 +0300

    подгрузка шапки и футера

--- frontend/js/loader.js
+++ frontend/js/loader.js
@@ -1,7 +1,7 @@
 let loadContent = () => {
-    let header = new Promise((resolve, ) => { $('.header-container').load('partial/header.html', () => resolve()); });
-    let footer = new Promise((resolve, ) => { $('.footer-container').load('partial/footer.html', () => resolve()); });
-    let modals = new Promise((resolve, ) => { $('.modals').load('partial/modals.html', () => resolve()); });
+    let header = new Promise((resolve, ) => { $('.header-container').load('/partial/header.html', () => resolve()); });
+    let footer = new Promise((resolve, ) => { $('.footer-container').load('/partial/footer.html', () => resolve()); });
+    let modals = new Promise((resolve, ) => { $('.modals').load('/partial/modals.html', () => resolve()); });
 
     return new Promise((resolve) => {
         Promise.all([header, footer, modals]).then(
